ccfd876b9fb7a5e3a014a355aeaf159ef47adf6e
ws55hu <shadrack.limbua@gmail.com>
2020-04-15 01:23:20 +0200

    Removed package-lock.json and tried yarn install

--- public/estimator.js
+++ public/estimator.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 /* eslint-disable no-restricted-properties */
@@ -176,3 +180,5 @@ var createTd = function createTd(input, tdBody) {
     tdBody.appendChild(tdRow);
   }
 };
+
+exports.default = covid19ImpactEstimator;
